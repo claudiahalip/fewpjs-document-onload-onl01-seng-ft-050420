112641481a5d46ba3ddf091ec43deb2ef50d19e8
claudiahalip <claudia4ro@yahoo.com>
2020-08-06 23:55:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Your code goes heredocument.
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let t = document.getElementById("text").innerHTML = 'This is really cool!';
+    
+    console.log("The DOM is loaded");
+})
